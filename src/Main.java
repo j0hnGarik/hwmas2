dae480a646be6488ee5ebad18b4366f23905de15
aleksandrteterin <sasha.teterin.04@mail.ru>
2025-03-03 00:15:40 +0300

    done hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("task 1");
+        int[] expenses = {1000, 2000, 1500, 3000, 2500};
+        int total = 0;
+        for (int expense : expenses) {
+            total += expense;
+        }
+        System.out.println("Сумма трат за месяц составила " + total + " рублей.");
+        System.out.println("task 2");
+        int[] expenses1 = {1000, 2000, 1500, 3000, 2500};
+        int minExpense = expenses[0];
+        int maxExpense = expenses[0];
 
+        for (int expense : expenses) {
+            if (expense < minExpense) {
+                minExpense = expense;
+            }
+            if (expense > maxExpense) {
+                maxExpense = expense;
+            }
+        }
+
+        System.out.println("Минимальная сумма трат за неделю составила " + minExpense + " рублей.");
+        System.out.println("Максимальная сумма трат за неделю составила " + maxExpense + " рублей.");
+        System.out.println("task 3");
+        int[] expenses2 = {1000, 2000, 1500, 3000, 2500};
+        int total1 = 0;
+        for (int expense2 : expenses2) {
+            total1 += expense2;
+        }
+        double average = (double) total1 / expenses2.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
+        System.out.println("task 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
     }
-}
+}}
